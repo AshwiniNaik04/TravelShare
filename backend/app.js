f2fd9184fff2a96717840b1b4b07bb3abc29c7a1
Ashwini Naik <naik999ashwini@gmail.com>
2026-04-18 23:02:13 -0700

    Updated backend for deployment

--- backend/app.js
+++ backend/app.js
@@ -97,10 +97,12 @@ mongoose
 
     console.log("DB Connected");
 
-    app.listen(5000, () => {
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
 
       console.log(
-        "Server running on port 5000"
+        `Server running on port ${PORT}`
       );
 
     });
